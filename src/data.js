d526e8186f8a3acc1927908eb5b78aac5b28fa85
M Fathurrohman Mauludin <fathurdiakfari97@gmail.com>
2023-10-09 17:50:09 +0700

    add phone number in sample data

--- src/data.js
+++ src/data.js
@@ -3,19 +3,22 @@ const getData = () => {
         {
             id: 1,
             name: 'Hatsune Miku',
-            tag: 'dimasmds',
+            phone_number: '+98325383291',
+            tag: 'miku-chan39',
             imageUrl: '/images/hatsune-miku.png',
         },
         {
             id: 2,
             name: 'Vladilena Milize',
-            tag: 'arifaizin',
+            phone_number: '+98325383292',
+            tag: 'lenamilize',
             imageUrl: '/images/vladilena-milize.jpg',
         },
         {
             id: 3,
             name: 'Elaina',
-            tag: 'rfajri27',
+            phone_number: '+98325383293',
+            tag: 'elaina_witch',
             imageUrl: '/images/elaina.jpg',
         },
     ];
