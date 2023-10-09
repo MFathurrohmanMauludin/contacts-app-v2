485a3f1cef1e6ed2cc8f40075daa153b9e7b79b3
M Fathurrohman Mauludin <fathurdiakfari97@gmail.com>
2023-10-09 17:47:32 +0700

    add images and sample data

---
+++ src/data.js
@@ -0,0 +1,24 @@
+const getData = () => {
+    return [
+        {
+            id: 1,
+            name: 'Hatsune Miku',
+            tag: 'dimasmds',
+            imageUrl: '/images/hatsune-miku.png',
+        },
+        {
+            id: 2,
+            name: 'Vladilena Milize',
+            tag: 'arifaizin',
+            imageUrl: '/images/vladilena-milize.jpg',
+        },
+        {
+            id: 3,
+            name: 'Elaina',
+            tag: 'rfajri27',
+            imageUrl: '/images/elaina.jpg',
+        },
+    ];
+}
+
+export { getData };
