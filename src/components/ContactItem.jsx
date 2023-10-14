2e9ff4e3fb3189c088c8fd6ccb4aee9c33c893c5
M Fathurrohman Mauludin <fathurdiakfari97@gmail.com>
2023-10-14 10:51:53 +0700

    update

--- src/components/ContactItem.jsx
+++ src/components/ContactItem.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ContactItemBody from './ContactItemBody';
 import ContactItemImage from './ContactItemImage';
+import DeleteButton from './DeleteButton';
 
 // Parent Component untuk ContactImage dan ContactItemBody
-const ContactItem = ({ imageUrl, name, phone_number, tag }) => {
+const ContactItem = ({ imageUrl, name, phone_number, tag, id, onDelete }) => {
     return (
         <div className="contact-item">
             <ContactItemImage imageUrl={imageUrl} />
             <ContactItemBody name={name} phone_number={phone_number} tag={tag} />
+            <DeleteButton id={id} onDelete={onDelete} />
         </div>
     );
 }
