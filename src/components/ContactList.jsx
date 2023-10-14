f4124f02705d4c393af02e3ab517e1baf97bd0a1
M Fathurrohman Mauludin <fathurdiakfari97@gmail.com>
2023-10-14 10:54:02 +0700

    update

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import ContactItem from './ContactItem';
 
 // Menampilkan contact berdasarkan banyaknya data yang diberikan (oleh data.js) melalui properti (props)
-const ContactList = ({ contacts }) => {
+const ContactList = ({ contacts, onDelete }) => {
     return (
         <div className="contact-list">
             {
                 contacts.map((contact) => (
-                    <ContactItem key={contact.id} {...contact} />
+                    <ContactItem key={contact.id} id={contact.id} onDelete={onDelete} {...contact} />
                 ))
             }
         </div>
